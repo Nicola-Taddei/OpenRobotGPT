e5822bf823393cc7a9154c11d2b48644cb74fd02
Nicola-Taddei <nicola.taddei01@gmail.com>
2024-03-22 12:09:05 +0100

    Workspace restyle

--- workspace/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nicola/repos/OpenRobotGPT/workspace/src
-# Build directory: /home/nicola/repos/OpenRobotGPT/workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
